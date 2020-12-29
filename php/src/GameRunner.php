4495cdcb51f2ed60edba155cea9b3efec2f835d7
Luciano Thoma <luchothoma@gmail.com>
2020-12-29 09:01:30 -0300

    Refactor to start point of kata for php. Src and Test folder, unit test suite runnable

---
+++ php/src/GameRunner.php
@@ -0,0 +1,23 @@
+<?php
+namespace Trivia;
+
+use Trivia\Game;
+
+$notAWinner;
+
+$aGame = new Game();
+
+$aGame->add("Chet");
+$aGame->add("Pat");
+$aGame->add("Sue");
+
+do {
+    $aGame->roll(rand(0,5) + 1);
+
+    if (rand(0,9) == 7) {
+    $notAWinner = $aGame->wrongAnswer();
+    } else {
+    $notAWinner = $aGame->wasCorrectlyAnswered();
+    }
+} while ($notAWinner);
+  
